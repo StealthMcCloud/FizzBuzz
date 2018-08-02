b1b9384c67f06e2ee18c8a7dcc20892f1d92b2da
Clinton Johnson <dragoon1246224@gmail.com>
2018-08-02 10:20:35 -0400

    Thanks Edwin for clarifing how this worked.

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
-let count1 = 0
-let count1_1 = document.getElementById("count1_1")
-while (count1 < 1000) {
-    count1++;
-    console.log(count1);
-
-    if (count1 == 2 % 0) { 
-        count1 += 2;
-        let count1 =  count1.bold();
-    }
-
-    else if (count1 == 3 % 0) { 
-        count1 += 3;
-        let count1 = count1.fontcolor("red");
-        console.log(count1);
-
-
-    }
-    count1_1.innerHTML = count1
+for (let count1 = 1; count1 <= 1000; count1++) {
+    
+    if (count1 % 2 == 0 && count1 % 3 == 0) {
+    document.write("<div class='bold, red'>" + count1 + "</div>")
 }
 
+    else if ( count1 % 2 == 0) { 
+        document.write("<div class='bold'>" + count1 + "</div>");
+    }
+
+    else if (count1 % 3 == 0) { 
+        document.write("<div class='red'>" + count1 + "</div>");
+    }
+
+    else {
+        document.write("<div>" + count1 + "</div>")
+    }
+}
+
+// for (let class1 = 1; i <= 1000; class1++) {
+//     const div = document.createElement("div");
+//     div.textcontent = class1;
+//     if (class1 % 3 ==0) {
+//         div.classList.add("red");
+//     }
+
+//     if (class1 % 2 == 0) {
+//         div.classList.add("bold");
+//     }
+// }
